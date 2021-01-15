3123ded5e893d768ec8c7becc015076bb4aea9ca
James <JamesWhite1911@protonmail.com>
2021-01-14 22:08:03 -0500

    mvp complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,10 +1,3 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
-//done
-
 import axios from "axios"; //import axios
 
 const entryPoint = document.querySelector('.cards'); //where we want to insert our html
@@ -14,9 +7,9 @@ function getUser() {
     .get('https://api.github.com/users/jameswhite1911') //grab the api
     .then((res) => {
       const data = res.data; //find our data
-      console.log("OUR DATA", res.data);
+      console.log("OUR DATA", res.data); //log the data we found
       const userCard = cardMaker({data}); //pass data to our card maker as an object
-      console.log(userCard);
+      console.log(userCard); //log our card heirarchy to make sure we have all the elements
       entryPoint.append(userCard); //append our card to the html where we want it
     })
     .catch((err) => {
@@ -24,58 +17,10 @@ function getUser() {
     });
 }
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
-
-    Skip to STEP 3.
-*/
-//we did this in logs throughout the program
-
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
-//done also
-
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
-
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
-
 const followersArray = ["tetondan", "dustinmyers", "justsml", "luishrd", "bigknell"];
-
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-//done
 
 function cardMaker(obj) {
   const info = obj.data;
-  console.log("login", info.login);
   const card = document.createElement('div');
   const image = document.createElement('img');
   const cardInfo = document.createElement('div');
@@ -116,8 +61,23 @@ function cardMaker(obj) {
   cardInfo.appendChild(following);
   cardInfo.appendChild(bio);
 
-
   return card;
 }
 
 getUser();
+
+function getArrayUsers() {
+  followersArray.forEach(user => {
+    axios
+    .get(`https://api.github.com/users/${user}`)
+    .then((res) => {
+      const data = res.data;
+      const userCard = cardMaker({data});
+      entryPoint.append(userCard);
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+  });
+}
+getArrayUsers();
